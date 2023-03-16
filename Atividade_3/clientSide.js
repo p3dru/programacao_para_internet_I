b1bf687057afdf8b62c1bc2bc8994b02f5f6479b
João Pedro <61830482+p3dru@users.noreply.github.com>
2023-03-16 09:43:05 -0300

    Inserção de comentários

--- Atividade_3/clientSide.js
+++ Atividade_3/clientSide.js
@@ -1,27 +1,39 @@
 const net = require('net');
 
+//cria um novo socket
 const socket = new net.Socket();
 
+//cria a lista/matriz do jogo da velha
 let board = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
 
+//mensagem que lança assim que o socket se conecta ao server
 socket.connect(3000, 'localhost', () => {
   console.log('Connected to server');
 });
 
+//printa os dados recebidos do servidor (quando o server manda com o write)
 socket.on('data', (data) => {
   console.log(data.toString());
 });
 
+//faz a função que verifica o movimento realizado pelo jogador atual (socket atual)
 function makeMove(move) {
+  //se o local enviado for diferente do "valor padrão do jogo", ele retorna como movimento inválido
   if (board[move] !== '-') {
     console.log('Invalid move');
     return;
   }
   
+  /*
+  caso o valor padrão esteja o local, é substituído pelo valor do player atual 
+  (que é alterado nas linhas 53 e 54 no arquivo serverSide.js)
+  */
   board[move] = 'X';
+  //transforma o valor recebido em string e envia para a matriz/lista do jogo da velha
   socket.write(move.toString());
 }
 
+//função para printar a lista/matriz do jogo da velha como uma matriz 3 X 3
 function printBoard() {
   let boardMessage = '';
   for (let i = 0; i < 9; i++) {
@@ -33,13 +45,18 @@ function printBoard() {
   console.log(boardMessage);
 }
 
+
+//solicita o valor para que seja passado como movimento para a matriz
 process.stdin.on('data', (data) => {
   const move = parseInt(data.toString(), 10);
+  //faz a verificação se o movimento é válido
   if (isNaN(move) || move < 0 || move > 8) {
     console.log('Invalid move');
     return;
   }
   
+  //faz o movimento baseado na resposta
   makeMove(move);
+  //printa a matriz 3 x 3
   printBoard();
 });
--- Atividade_3/serverSide.js
+++ Atividade_3/serverSide.js
@@ -1,55 +1,82 @@
 const net = require('net');
 
+//cria o servidor
 const server = net.createServer();
 
+//cria uma lista de sockets
 let sockets = [];
+//cria a "matriz" do jogo da velha
 let board = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
+//cria o player atual como X
 let currentPlayer = 'X';
 
+//assim que o socket se conecta, uma mensagem no servidor é lançada
 server.on('connection', (socket) => {
   console.log('New client connected');
 
+  //na linha abaixo o socket que se conectou ao servidor, e inserido na lista de sockets
   sockets.push(socket);
 
+  //se a lista de sockets chegar ao tamanho 2, o jogo se inicia e uma mensagem é enviada aos jogadores
   if (sockets.length === 2) {
     broadcast(`Player 1 (X) turn`);
   }
 
+  //O on 'data', é para tratar dadps recebidos pelo servidor (que o socket envia com write)
   socket.on('data', (data) => {
+    //transforma a resposta do socket em inteiro (para que se adeque à posição da matriz de 0 à 8)
+    if (data.toString() === 'end'){
+        sockets.forEach((s) => s.destroy());
+    }
+
     const move = parseInt(data, 10);
     
+    //verifica se o movimento realizado é valido
     if (isNaN(move) || move < 0 || move > 8 || board[move] !== '-') {
       socket.write('Invalid move\n');
       return;
     }
     
+    //aqui atualiza a lista/matriz do jogo da velha, com o valor do player atual
     board[move] = currentPlayer;
     
+    //verifica vencedor passando como parâmetro o player atual
     if (checkWinner(currentPlayer)) {
+      //envia a mensagem para o vencedor
       broadcast(`${currentPlayer} wins!`);
       broadcast('Game over');
+      //para cada socket na lista de sockets, eles serão destruídos após a checagem caso o jogo tenha terminado
       sockets.forEach((s) => s.destroy());
       return;
     }
     
+    /*
+    aqui altera o player atual, após a jogada feita na linha 37, o player será alterado para 'O' se
+    o player atual é o 'X' e para 'X' se o atual é o 'O', operador ternário
+    */
     const nextPlayer = currentPlayer === 'X' ? 'O' : 'X';
     currentPlayer = nextPlayer;
     
+    //manda a informação de quem é o jogador que ira jogar
     broadcast(`Player ${nextPlayer} (${currentPlayer}) turn`);
     broadcastBoard();
   });
   
+  //quando o socket for desconectado
   socket.on('close', () => {
     console.log('Client disconnected');
+    //envia um a mensagem aos outros sockets informando que um foi desconectado
     sockets = sockets.filter((s) => s !== socket);
     broadcast('Other player disconnected');
     sockets.forEach((s) => s.destroy());
   });
 
+  //envia uma mensagem para cada socket na lista de sockets
   function broadcast(message) {
     sockets.forEach((s) => s.write(`${message}\n`));
   }
 
+  //envia a lista/matriz do jogo da velha como uma matriz 3 X 3
   function broadcastBoard() {
     let boardMessage = '';
     for (let i = 0; i < 9; i++) {
@@ -60,7 +87,8 @@ server.on('connection', (socket) => {
     }
     broadcast(boardMessage);
   }
-
+  
+  //verifica se o player atual é o vencedor
   function checkWinner(player) {
     const rows = [
       [0, 1, 2],
@@ -73,6 +101,10 @@ server.on('connection', (socket) => {
       [2, 4, 6],
     ];
     
+    /*
+    pega todos as colunas que possivelmente fariam um vencedor e analizam se algumas dessas listas tem 
+    o mesmo símbolo em sequencia, se sim, o jogo tem um vencedor
+    */
     for (let i = 0; i < rows.length; i++) {
       const [a, b, c] = rows[i];
       if (board[a] === player && board[b] === player && board[c] === player) {
