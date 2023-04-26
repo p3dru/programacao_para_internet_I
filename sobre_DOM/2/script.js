2eede8ad62ead565c33460b7c6665afe82381470
João Pedro <61830482+p3dru@users.noreply.github.com>
2023-04-26 12:19:10 -0300

    Delete sobre_DOM directory

--- sobre_DOM/2/script.js
+++
@@ -1,5 +0,0 @@
-var button = document.getElementById("link");
-console.log(`O link encontrado por 'document.getElementById("link")' foi o: ${button}`);
-
-var primeiroParagrafo = document.getElementsByTagName('p')[0].innerText;
-console.log(`O encontrado pela busca 'document.getElementsById('p')[0].innerText' foi: ${primeiroParagrafo}`);
